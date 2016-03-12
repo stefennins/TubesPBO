dd12b07406d7abc94782ae6cbe548b89b601b5e0
Stephanus Prasetyo <stefennins@gmail.com>
2016-03-12 20:11:15 +0700

    Commit Class

--- Tubes_PBO/src/tubes_pbo/Petugas.java
+++ Tubes_PBO/src/tubes_pbo/Petugas.java
@@ -9,6 +9,20 @@ package tubes_pbo;
  *
  * @author Ivanarhea
  */
-public class Petugas {
-    
+public class Petugas extends Orang{
+	private String idPetugas;
+
+	public Petugas(String idPetugas, String nama, String username, String password, String alamat){
+            super(nama, username, password, alamat);
+            this.idPetugas = idPetugas;
+            this.idPetugas = idPetugas;
+	}
+
+	public String idPetugas(){
+		return idPetugas;
+	}
+
+	public void setIdPetugas(String idPetugas){
+		this.idPetugas = idPetugas;
+	}
 }
