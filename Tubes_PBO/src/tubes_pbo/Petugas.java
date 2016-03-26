3ac7145e5ddba5e1bd6bf41c2d5fe8e22368ec1b
Stephanus Prasetyo <stefennins@gmail.com>
2016-03-26 22:12:15 +0700

    Update Petugas.java

--- Tubes_PBO/src/tubes_pbo/Petugas.java
+++ Tubes_PBO/src/tubes_pbo/Petugas.java
@@ -1,24 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tubes_pbo;
+package tubespbo;
 
-/**
- *
- * @author Ivanarhea
- */
 public class Petugas extends Orang{
 	private String idPetugas;
 
 	public Petugas(String idPetugas, String nama, String username, String password, String alamat){
             super(nama, username, password, alamat);
             this.idPetugas = idPetugas;
-            this.idPetugas = idPetugas;
 	}
 
-	public String idPetugas(){
+	public String getIdPetugas(){
 		return idPetugas;
 	}
 
