19c5347430b2f0a07fbc009f6f8cc00817ffd78f
Stephanus Prasetyo <stefennins@gmail.com>
2016-03-26 22:11:57 +0700

    Update Perpustakaan.java

--- Tubes_PBO/src/tubes_pbo/Perpustakaan.java
+++ Tubes_PBO/src/tubes_pbo/Perpustakaan.java
@@ -3,19 +3,24 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tubes_pbo;
+package tubespbo;
 
-import.java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
- * @author Ivanarhea
+ * @author admin
  */
 public class Perpustakaan {
+    
     private String nama;
+    private int jmlPeminjaman = 3;
     private ArrayList<Buku> arrayBuku;
     private ArrayList<Anggota> arrayAnggota;
     private ArrayList<Petugas> arrayPetugas;
     private ArrayList<Peminjaman> arrayPeminjaman;
+    Scanner scanString = new Scanner(System.in);
     
     public Perpustakaan(String nama){
         this.nama = nama;
@@ -38,11 +43,7 @@ public class Perpustakaan {
     public void setNama(String nama) {
         this.nama = nama;
     }
-    ///menambah anggota baru
-    public void addAnggota(Anggota anggota){
-        arrayAnggota.add(anggota);
-    }
-    ///menambah buku ke perpustakaan
+    
     public void tambahBuku(Buku buku){
         arrayBuku.add(buku);
     }
@@ -53,7 +54,6 @@ public class Perpustakaan {
     public ArrayList<Buku> getArrayBuku() {
         return arrayBuku;
     }
-
     /**
      * @param arrayBuku the arrayBuku to set
      */
@@ -126,7 +126,16 @@ public class Perpustakaan {
     
     public Petugas getPetugas(String idPetugas){
         for(Petugas p : arrayPetugas){
-            if(p.idPetugas().equals(idPetugas)){
+            if(p.getIdPetugas().equals(idPetugas)){
+                return p;
+            }
+        }
+        return null;
+    }
+    
+    public Petugas getPetugas(String username, String password){
+        for(Petugas p : arrayPetugas){
+            if((p.getUsername().equals(username)) && (p.getPassword().equals(password))){
                 return p;
             }
         }
@@ -142,6 +151,15 @@ public class Perpustakaan {
         return null;
     }
     
+    public Anggota getAnggotaUsername(String username){
+        for(Anggota a : arrayAnggota){
+            if(a.getUsername().equals(username)){
+                return a;
+            }
+        }
+        return null;
+    }
+    
     public Peminjaman getPeminjaman(String idPeminjaman){
         for(Peminjaman p : arrayPeminjaman){
             if(p.getIdPeminjaman().equals(idPeminjaman)){
@@ -149,6 +167,91 @@ public class Perpustakaan {
             }
         }
         return null;
+    }
+    
+    public int getJumlahAnggota() {
+        return arrayAnggota.size();
+    }
+    
+    public Anggota searchIdAnggota(String id) {
+        for(Anggota a : arrayAnggota) {
+            if(a.getIdAnggota().equals(id)){
+                return a;
+            }
+        }
+        return null;
+    }
+    
+    //public boolean ubahKondisiBuku(String kondisi, Buku b){
+        
+    //}
+    
+    public Anggota getAnggotaByUserPass(String username, String password) {
+        for(Anggota a : arrayAnggota){
+            if(a.getUsername().equals(username) && a.getPassword().equals(password))
+            return a;
+        }
+        return null;
+    }
+    
+    public boolean cekStatus(String username, String password){
+        for(Anggota a : arrayAnggota){
+            if(a.getUsername().equals(username)){
+                if(a.getPassword().equals(password)){
+                    return true;
+                }
+                return false;
+            }
+        }
+        return false;
+    }
+    
+    public void inputPeminjaman(String tgl, Anggota anggota, Petugas petugas){
+        Peminjaman p = new Peminjaman("P"+jmlPeminjaman, tgl, 6, anggota, petugas);
+        boolean tambah = true;
+        while(tambah){
+            System.out.print("Masukkan idBuku : ");
+            String id = scanString.nextLine();
+            Buku b = cariBuku(id);
+            if(b!=null){
+                p.addBuku(b);
+            } else {
+                System.out.println("Buku TIdak Ditemukan");
+            }
+            System.out.println("Selesai? (y/t)");
+            String i = scanString.nextLine();
+            if(i.equals("y")){
+                tambah = false;
+            }
+        }
+        arrayPeminjaman.add(p);
+    }
+    
+    public void inputPeminjaman1(String tgl, Anggota anggota, Petugas petugas, ArrayList<Buku> b){
+        Peminjaman p = new Peminjaman("P"+jmlPeminjaman, tgl, 6, anggota, petugas);
+        for(Buku buku : b){
+            p.addBuku(buku);
+        }
+        arrayPeminjaman.add(p);
+    }
+    
+    public Buku cariBuku(String idBuku){
+        for(Buku b : arrayBuku){
+            if(b.getIdBuku().equals(idBuku)){
+                return b;
+            }
+        }
+        return null;
+    }
+    
+    public ArrayList<Peminjaman> getPeminjaman(Anggota a){
+        ArrayList<Peminjaman> p = new ArrayList<>();
+        for(Peminjaman p1 : arrayPeminjaman){
+            if(p1.getPeminjam().equals(a)){
+                p.add(p1);
+            }
+        }
+        return p;
     }
     
 }
