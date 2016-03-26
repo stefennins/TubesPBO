c2aa7f2ad634c79dae66eac6f823e955f13c7044
Stephanus Prasetyo <stefennins@gmail.com>
2016-03-26 22:09:18 +0700

    Update Anggota.java

--- Tubes_PBO/src/tubes_pbo/Anggota.java
+++ Tubes_PBO/src/tubes_pbo/Anggota.java
@@ -1,35 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tubes_pbo;
+package tubespbo;
 
 import java.util.ArrayList;
-/**
- *
- * @author Ivanarhea
- */
+
 public class Anggota extends Orang {
-    private String idAnggota;
-    
-    public Anggota(String idAnggota, String nama, String username, String password, String alamat){
-        super(nama, alamat, username, password);
-        this.idAnggota = id Anggota;
-    }
+	private String idAnggota;
 
-    /**
-     * @return the idAnggota
-     */
-    public String getIdAnggota() {
-        return idAnggota;
-    }
-
-    /**
-     * @param idAnggota the idAnggota to set
-     */
-    public void setIdAnggota(String idAnggota) {
-        this.idAnggota = idAnggota;
-    }
-    
+	public Anggota(String idAnggota, String nama, String username, String password, String alamat){
+		super(nama, alamat, username, password);
+                this.idAnggota = idAnggota;
+	}
+        
+        public String getIdAnggota(){
+            return idAnggota;
+        }
+        
+        public void setIdAnggota(String idAnggota){
+            this.idAnggota = idAnggota;
+        }
 }
