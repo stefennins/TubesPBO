86c433ee89636aa4c6b454964a284109479c8b95
Stephanus Prasetyo <stefennins@gmail.com>
2016-03-26 22:11:01 +0700

    Update Orang.java

--- Tubes_PBO/src/tubes_pbo/Orang.java
+++ Tubes_PBO/src/tubes_pbo/Orang.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tubes_pbo;
+package tubespbo;
 
-/**
- *
- * @author Indri
- */
 public abstract class Orang {
     private String nama;
     private String alamat;
